3bfec9a0bfa4a250d2c46c4866b5ffe134a716fb
Ian Beck <ian@onecrayon.com>
2012-03-08 12:39:10 -0800

    Re-implemented Update Image Size action

--- Scripts/image_size.py
+++ Scripts/image_size.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
+
 '''
 @author: Sergey Chikuyonok (serge.che@gmail.com)
 '''
 from subprocess import Popen, PIPE
-import urllib
-import os
+import os, sys
 
 import re
-import tea_actions as tea
 
 def replace_or_append(img_tag, attr_name, attr_value):
 	"""
@@ -22,49 +22,24 @@ def replace_or_append(img_tag, attr_name, attr_value):
 	else:
 		return re.sub(r'\s*(\/?>)$', ' ' + attr_name + '="' + attr_value + r'" \1', img_tag)
 
-def find_image(context):
+def find_images(text):
 	"""
-	Find image tag under caret
- 	@return Image tag and its indexes inside editor source
+	Find image tags in selection
+ 	@return Image tags and their indexes inside editor source
 	"""
-	rng = tea.get_ranges(context)[0]
-	caret_pos = rng.location
-	text = context.string()
-	start_ix = -1
-	end_ix = -1
+	imgRE = re.compile(r'<img (?:[-_a-zA-Z0-9:]+=(?:".*?(?<!\\)"|\'.*?(?<!\\)\'|\S+)\s*)+/?>', re.I)
 	
-	# find the beginning of the tag
-	while caret_pos >= 0:
-		if text[caret_pos] == '<':
-			if text[caret_pos:caret_pos + 4].lower() == '<img':
-				# found the beginning of the image tag
-				start_ix = caret_pos
-				break
-			else:
-				# found some other tag
-				return None
-		caret_pos -= 1
-			
-	# find the end of the tag 
-	caret_pos = rng.location
-	ln = len(text)
-	while caret_pos <= ln:
-		if text[caret_pos] == '>':
-			end_ix = caret_pos + 1
-			break
-		caret_pos += 1
+	images = []
+	for match in imgRE.finditer(text):
+		images.append({
+			'start': match.start(),
+			'end': match.end(),
+			'tag': match.group(0)
+		})
 	
-	
-	if start_ix != -1 and end_ix != -1:
-		return {
-			'start': start_ix,
-			'end': end_ix,
-			'tag': text[start_ix:end_ix]
-		}
-	
-	return None
+	return images
 
-def get_image_size(context, img):
+def get_image_size(img):
 	"""
 	Returns size of image in <img>; tag
  	@param img: Image tag
@@ -72,12 +47,11 @@ def get_image_size(context, img):
 	"""
 	m = re.search(r'src=(["\'])(.+?)\1', img, re.IGNORECASE)
 	if m:
-		src = get_absolute_uri(context, m.group(2))
+		src = get_absolute_uri(m.group(2))
 		if not src:
 			return None
 		try:
 			raw_output = Popen('sips -g pixelWidth -g pixelHeight "%s"' % src, stdout=PIPE, shell=True).communicate()[0]
-			tea.log(raw_output)
 			return {
 				'width': re.search(r'pixelWidth:\s*(\d+)', raw_output).group(1),
 				'height': re.search(r'pixelHeight:\s*(\d+)', raw_output).group(1)
@@ -87,10 +61,8 @@ def get_image_size(context, img):
 		
 	return None
 
-def get_absolute_uri(context, img_path):
-	file_uri = urllib.unquote(context.documentContext().fileURL().absoluteString())
-	# remove protocol
-	file_uri = re.sub(r'^\w+://\w+', '', file_uri)
+def get_absolute_uri(img_path):
+	file_uri = os.environ['EDITOR_PATH']
 	
 	if img_path[0] == '/':
 		img_path = img_path[1:]
@@ -101,7 +73,6 @@ def get_absolute_uri(context, img_path):
 			
 		abs_image_path = os.path.join(head, img_path)
 		abs_image_path = os.path.normpath(abs_image_path)
-		tea.log('testing ' + abs_image_path)
 		if os.path.exists(abs_image_path):
 			return os.path.join(abs_image_path)
 		
@@ -110,18 +81,15 @@ def get_absolute_uri(context, img_path):
 		
 	return None
 
-def act(context):
-	image = find_image(context)
-	if image:
-		size = get_image_size(context, image['tag'])
-		if size:
-			new_tag = replace_or_append(image['tag'], 'width', size['width'])
-			new_tag = replace_or_append(new_tag, 'height', size['height'])
-			
-			rng = tea.get_ranges(context)[0]
-			tea.insert_snippet_over_range(context, new_tag, tea.new_range(image['start'], image['end'] - image['start']), 'Update Image Size')
-			tea.set_selected_range(context, rng)
-			return True
-	
-	return False	
+# Actual script
+text = sys.stdin.read()
+images = find_images(text)
+for image in reversed(images):
+	size = get_image_size(image['tag'])
+	if size:
+		new_tag = replace_or_append(image['tag'], 'width', size['width'])
+		new_tag = replace_or_append(new_tag, 'height', size['height'])
 		
+		text = text[0:image['start']] + new_tag + text[image['end']:]
+
+sys.stdout.write(text)
